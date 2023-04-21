deab4a3a1fb0e8d630230c0691682d887e270ba4
JorgeA-Extrados <jorge.albornos@extrados.ar>
2023-04-21 14:45:04 -0300

    login

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose'
 import { ProveedorModule } from './proveedor/proveedor.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost/products-nest-tutorial'), ProveedorModule],
+  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost/products-nest-tutorial'), ProveedorModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
